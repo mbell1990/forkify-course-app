e85da33d9f44de2be789ad4f5ac2681d76142420
Matthew Bell <m.r.bell@outlook.com>
2023-02-10 07:24:46 +0000

    removed polyfilling imports

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,9 +7,10 @@ import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+//import 'core-js/stable';
+//import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
+//import { add } from 'lodash-es';
 
 // if (module.hot) {
 //   module.hot.accept(); // hot reloading from parcel
